995c262d7e496d37650052de8e60897a80934e78
SuchatBiggs <bigbig2540@hotmail.com>
2017-10-25 06:46:42 +0000

    add index.ejs login.ejs | add index route

--- app.js
+++ app.js
@@ -7,17 +7,28 @@ var express     = require("express"),
     LocalStrategy = require("passport-local"),
     methodOverride = require("method-override"),
     User        = require("./models/user")
-    
+
+//ROUTES SETUP
+var indexRoutes      = require("./routes/index")
+
+//DATABASE SETUP
 var url = process.env.DATABASEURL || "mongodb://localhost/bumadb";
 mongoose.connect(url);
-    
-    
+
+
+//SETUP APPLICATION    
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
 
+// PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret: "CE KMITL DEK AMERICANO",
+    resave: false,
+    saveUninitialized: false
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
@@ -25,9 +36,9 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 
-app.get("/",function(req,res){
-     res.send("asf");
-})
+
+
+app.use("/", indexRoutes);
 
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("BUMA APP START !!");
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var router  = express.Router();
+var passport = require("passport");
+var User = require("../models/user");
+
+//root route
+router.get("/", function(req, res){
+    res.render("index");
+});
+
+
+module.exports = router;
