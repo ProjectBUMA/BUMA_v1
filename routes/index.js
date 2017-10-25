3ea1bc8a6a7b2282eb0e85f7852e5ba9041b69be
SuchatBiggs <bigbig2540@hotmail.com>
2017-10-25 07:51:04 +0000

    add singup and login

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,38 @@ router.get("/", function(req, res){
     res.render("index");
 });
 
+// show register form
+router.get("/register", function(req, res){
+   res.render("register"); 
+});
+
+//handle sign up logic
+router.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            req.flash("error", err.message);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+           req.flash("success", "Login Success " + user.username);
+           res.redirect("/"); 
+        });
+    });
+});
+
+//show login form
+router.get("/login", function(req, res){
+   res.render("login"); 
+});
+
+//handling login logic
+router.post("/login", passport.authenticate("local", 
+    {
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login"
+    }), function(req, res){
+});
+
 
 module.exports = router;
