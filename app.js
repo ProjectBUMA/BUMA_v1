e7b4b55a0b4b40279dc8e0569e789c44f3ce5a4d
SuchatBiggs <bigbig2540@hotmail.com>
2017-10-25 06:19:30 +0000

    add app.js main structure and user schema

--- app.js
+++ app.js
@@ -1,2 +1,34 @@
 var express     = require("express"),
-    app         = express()
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    flash       = require("connect-flash"),
+    passport    = require("passport"),
+    LocalStrategy = require("passport-local"),
+    methodOverride = require("method-override"),
+    User        = require("./models/user")
+    
+var url = process.env.DATABASEURL || "mongodb://localhost/bumadb";
+mongoose.connect(url);
+    
+    
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+app.use(methodOverride("_method"));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+app.get("/",function(req,res){
+     res.send("asf");
+})
+
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("BUMA APP START !!");
+});
